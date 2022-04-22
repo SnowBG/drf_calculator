e05f3cf1e3a17b5c1d9cdf85bfe6ff7cbcac6821
snowbg <snowbg@gmail.com>
2022-04-22 13:12:11 -0300

    Created OperationHistoryViewSet.

--- calculator/operations/views.py
+++ calculator/operations/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from calculator.operations import serializers, models
+
+
+class OperationHistoryViewSet(viewsets.ModelViewSet):
+    """Endpoint to view operations's data."""
+
+    queryset = models.User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
